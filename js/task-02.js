d7bd70f3ab5d69814d5b05ec33f99570d9083e6a
Karolfm12 <karolfm12@onet.eu>
2023-12-31 20:57:14 +0100

    taks2passed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulIngredients = document.querySelector("#ingredients");
+
+for (const item of ingredients) {
+  const createItem = document.createElement("li");
+  createItem.textContent = item;
+  ulIngredients.append(createItem);
+}
