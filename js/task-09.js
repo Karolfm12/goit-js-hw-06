5d7057d538e709ea85c8f3af94923e900fe2b494
Karolfm12 <karolfm12@onet.eu>
2024-01-01 16:32:03 +0100

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,6 @@
 const button = document.querySelector(".change-color");
 const body = document.querySelector("body");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
