07192bc88decf1ecaa427211744257876a7d256f
Karolfm12 <karolfm12@onet.eu>
2024-01-01 15:46:32 +0100

    task-08 passed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  if (email === "" || password === "") {
+    alert("wszystkie pola powinny zostać wypełnione");
+  } else {
+    console.log(`${form.elements.email.name}: ${email}
+${form.elements.password.name}: ${password}`);
+  }
+  form.reset();
+});
